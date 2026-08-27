02508ce9326aaa2e94879a645cad0060a187dfcc
x56001682-debug <x56001682@gmail.com>
2026-08-26 21:39:14 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static('.'));
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
